f0be962083351128e1140df1ffb0fd33f1778f15
addienf <addienaf2001@gmail.com>
2022-01-03 08:28:44 +0700

    Menambah fungsionalitas add dan sub

---
+++ src/com/oop/git/Aritmatika.java
@@ -0,0 +1,16 @@
+package com.oop.git;
+
+public class Aritmatika {
+	//Deveoper 1
+	public double add (int a, int b) {
+		return a + b;
+	}
+	
+	public double sub (int a, int b) {
+		return a - b;
+	}
+	
+	//Developer 2 (mult)
+	
+	//Developer 2 (div)
+}
